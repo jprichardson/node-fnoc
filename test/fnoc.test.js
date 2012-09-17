d5d5b70f3c5b7f216b698a2d7422ffe8655ee01c
JP Richardson <jprichardson@gmail.com>
2012-09-17 04:18:40 -0500

    Rewrote module from CoffeeScript to JavaScript. Closes #1. Rewrote to async.

---
+++ test/fnoc.test.js
@@ -0,0 +1,65 @@
+var testutil = require('testutil')
+  , path = require('path-extra')
+  , fm = require('faux-module');
+
+OWD = process.cwd();
+
+describe('fnoc', function() {
+  describe('+ fnoc()', function() {
+    it('should load all of the valid config files', function  (done) {
+      fm.getConfigs(function (err, configs) {
+        var malformedJsonPath = path.resolve('test/node_modules/faux-module/config/malformed.json')
+        T (err)
+        T (err[malformedJsonPath]) //error on parsing file
+        
+        T (configs.package)
+        T (configs.shopping)
+        T (configs.database)
+        F (configs.malformed)
+
+        T (configs.shopping.server === 'localhost')
+        T (configs.package.name === 'faux-module')
+        T (configs.database.production.name === 'myapp_production')
+
+        done();
+      })
+    })
+
+    it('should load all of the valid config files from a different directory', function (done) {
+      var malformedJsonPath = path.resolve('test/node_modules/faux-module/config/malformed.json')
+      process.chdir(path.tempdir());
+      fm.getConfigs(function (err, configs) {
+        T (err)
+        T (err[malformedJsonPath]) //error on parsing file
+        
+        T (configs.package)
+        T (configs.shopping)
+        T (configs.database)
+        F (configs.malformed)
+
+        T (configs.shopping.server === 'localhost')
+        T (configs.package.name === 'faux-module')
+        T (configs.database.production.name === 'myapp_production')
+        process.chdir(OWD);
+
+        done();
+      })
+    })
+
+    it('should load the environment specific valid config files', function (done) {
+      T (process.env.NODE_ENV === 'test')
+      fm.getConfigs(function (err, configs) {
+        T (err)
+
+        var envConfigs = configs.env();
+        T (envConfigs.shopping.server === 'localhost')
+        T (envConfigs.package.name === 'faux-module')
+        T (envConfigs.database.name === 'myapp_test')
+      
+        done();
+      })
+    })
+
+
+  })
+})
